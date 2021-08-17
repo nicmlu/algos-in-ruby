69a357004f418ee698fd0ecae7c9aa14220a576e
nicmlu <NicoleMLucien@gmail.com>
2021-08-17 11:19:00 -0400

    example inputs and expected outputs

--- algo_practice/binary_search.rb
+++ algo_practice/binary_search.rb
@@ -2,6 +2,14 @@
 # Given a sorted array of integers, return the index of the given key. Return -1 if the key is not found.
 
 # Use the divide and conquer strategy
+# Examples:
+    # Input: [2, 3, 5, 7, 11, 14, 21, 27, 29, 31, 32]
+    # Key: 21
+    # Output: 6
+
+    # Input: [2]
+    # Key: 4
+    # Output: -1 
 
 # Recursive Solution
 # Time: O(log n)
@@ -14,3 +22,5 @@
 # The index at low remains the same.
 # If the element at mid is less than the key, then change low to mid + 1. The index at high remains the same.
 # When low is greater than high, the key doesn’t exist and -1 is returned.
+
+
